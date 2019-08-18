910ff6917a1354e22c3c7b3c4d30b161c2b2634d
njorothegniuos <njosh51@gmail.com>
2019-08-18 13:12:30 +0300

    First commit

---
+++ src/main/java/com/MBASE/GatewayBookhubApplication.java
@@ -0,0 +1,13 @@
+package com.MBASE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayBookhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayBookhubApplication.class, args);
+	}
+
+}
